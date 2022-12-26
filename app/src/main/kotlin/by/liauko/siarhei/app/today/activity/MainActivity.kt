b2cb82680ca6646b092f14f0b447b6db15f4f15d
Sergey Levko <11446801+sergey-levv@users.noreply.github.com>
2022-12-26 13:26:12 +0300

    TA-26 - Make current day of the year visible on lock screen and move to silent notification area (#36)

--- app/src/main/kotlin/by/liauko/siarhei/app/today/activity/MainActivity.kt
+++ app/src/main/kotlin/by/liauko/siarhei/app/today/activity/MainActivity.kt
@@ -123,9 +123,9 @@ class MainActivity : AppCompatActivity() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "channel_name"
-            val descriptionText = "Channel_description"
-            val importance = NotificationManager.IMPORTANCE_HIGH
+            val name = getString(R.string.notification_channel_name)
+            val descriptionText = getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_LOW
             val channel = NotificationChannel(getString(R.string.notification_channel_id), name, importance).apply {
                 description = descriptionText
             }
